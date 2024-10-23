de59ead9cb95a0eace9c0b25002ba018a70e0ea7
Enqd <2891275548@qq.com>
2024-10-23 18:09:42 +0800

    main

--- A/grayscale_histogram.py
+++ A/grayscale_histogram.py
@@ -4,22 +4,22 @@ import numpy as np
 
 # 计算直方图函数
 def histCover(img):
-    # 直方图计算
-    hist = cv.calcHist([img], [0], None, [256], [0, 255])
-    # 图片均衡化处理
-    equ = cv.equalizeHist(img)    
-    # 两张图片并排显示
-    cv.imshow('equalization', np.hstack((img, equ)))
+    equ = cv.equalizeHist(img)
+
+    cv.imshow("src", img)
+    cv.imshow("result", equ)
+    
     cv.waitKey(0)
     cv.imwrite("../result/A/equ_contrast.jpg", np.hstack((img, equ)))
 
     # 均衡化后直方图计算，显示均衡化对比效果
-    hist_equ = cv.calcHist([equ], [0], None, [256], [0, 255])
     plt.figure("对比图", figsize=(8, 4))
     plt.subplot(121)
-    plt.plot(hist)
+    plt.hist(img.ravel(), 256)
     plt.subplot(122)
-    plt.plot(hist_equ)
+    plt.hist(equ.ravel(), 256)
     plt.xlim([0, 255])
     plt.savefig("../result/A/histogram_contrast.jpg")
     plt.show()
+    
+    
--- A/main.py
+++ A/main.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     # 将图片缩小，便于查看
     width, height = img.shape[:2][::-1]
     img_resize = cv.resize(img, (int(width*0.5), int(height*0.5)), interpolation=cv.INTER_CUBIC) 
-    # 灰度级切片展示两张图片对比
-    show_two_pictures(img_resize, grayscale_slice(img_resize, 100))
-    # 位平面切片展示8张位平面处理图
-    bit_plain_slice(img_resize)
+    # # 灰度级切片展示两张图片对比
+    # show_two_pictures(img_resize, grayscale_slice(img_resize, 100))
+    # # 位平面切片展示8张位平面处理图
+    # bit_plain_slice(img_resize)
     # 灰度直方图
     histCover(img_resize)
 
