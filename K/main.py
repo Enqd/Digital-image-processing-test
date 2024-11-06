981b259ee8e58e147e6a25f4270728a0a096c45e
Enqd <2891275548@qq.com>
2024-11-06 12:08:29 +0800

    main

--- K/main.py
+++ K/main.py
@@ -14,7 +14,7 @@ axe[0].imshow(img_g, cmap = 'gray'), axe[0].set_title('gaussian_noise')
 # 算数平均滤波器
 img_f = filter.arithmetic_average_filter(img_g, 3, 3)
 axe[1].imshow(img_f, cmap = 'gray'), axe[1].set_title('gaussian_noise_clear')
-plt.savefig("../result/K/gaussian_noise_clear.jpg")
+plt.savefig("../result/K/gaussian_noise_clear.jpg", bbox_inches='tight', pad_inches=0)
 plt.show()
 
 # 均匀噪声与清除后前后对比
@@ -23,7 +23,7 @@ axe[0].imshow(img_u, cmap = 'gray'), axe[0].set_title('uniform_noise')
 # 几何均值滤波器
 img_f = filter.Geometric_mean_filter(img_u, 3, 3)
 axe[1].imshow(img_f, cmap = 'gray'), axe[1].set_title('uniform_noise_clear')
-plt.savefig("../result/K/uniform_noise_clear.jpg")
+plt.savefig("../result/K/uniform_noise_clear.jpg", bbox_inches='tight', pad_inches=0)
 plt.show()
 
 # 椒盐噪声与清除后前后对比
@@ -32,5 +32,5 @@ axe[0].imshow(img_s, cmap = 'gray'), axe[0].set_title('salt_pepper_noise')
 # 最大值滤波器
 img_f = filter.maximum_filter(img_s, 3, 3)
 axe[1].imshow(img_f, cmap = 'gray'), axe[1].set_title('salt_pepper_noise_clear')
-plt.savefig("../result/K/salt_pepper_noise_clear.jpg")
+plt.savefig("../result/K/salt_pepper_noise_clear.jpg", bbox_inches='tight', pad_inches=0)
 plt.show()
